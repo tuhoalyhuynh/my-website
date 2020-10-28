2c9d0bd732852c4c08bc74c464a5c14d3f20bffa
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-10-28 09:59:42 -0400

    [js] Add code to add element to html

---
+++ app.js
@@ -0,0 +1,36 @@
+{/* <nav class="navbar fixed-top">
+<a class="bungie-logo" href="#">
+  <img src="https://www.bungie.net/img/bungie_styleguide/icon-shield.png?cv=3983621215&av=237140785" width="40" height="40" class="d-inline-block align-top" alt="" loading="lazy">
+  Bungie
+</a>
+<button class="btn btn-sm btn-outline-secondary" id="play" type="button">PLAY DESTINY 2</button>
+
+</nav> */}
+
+// const navbar = document.createElement('nav');
+// navbar.classList.add('navbar', 'fixed-top');
+
+// const bungieLogo = document.createElement('a');
+// bungieLogo.classList.add('bungie-logo');
+// bungieLogo.setAttribute('href', '#');
+
+// const img = document.createElement('img');
+// img.setAttribute('src', 'https://www.bungie.net/img/bungie_styleguide/icon-shield.png?cv=3983621215&av=237140785');
+// img.setAttribute('width', '40');
+// img.setAttribute('height', '40');
+// img.classList.add('d-inline-block', 'align-top');
+// img.setAttribute('loading', 'lazy')
+
+// bungieLogo.appendChild(img);
+
+// navbar.appendChild(bungieLogo);
+
+// const button = document.createElement('button');
+// button.classList.add('btn', 'btn-sm', 'btn-outline-secondary');
+// button.setAttribute('id', 'play');
+// button.setAttribute('type', 'button');
+// button.textContent = 'PLAY DESTINY 2';
+
+// navbar.appendChild(button);
+
+// document.querySelector('.container').appendChild(navbar)
